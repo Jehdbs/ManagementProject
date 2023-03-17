9a347feea5c4017b2fbab731fbe222a8210faa22
jehdbs <bigdoyun@naver.com>
2023-03-18 00:42:47 +0900

    remove hello.java added MenuManager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1. Add Subject");
+		System.out.println("2. Delete Subject");
+		System.out.println("3. Edit Subject");
+		System.out.println("4. View Subject");
+		System.out.println("5. Exit");
+	}
+
+}
