57b27ad91f1c47ad6ccd5394752bd6ff4ef402a0
Jehdbs <126933837+Jehdbs@users.noreply.github.com>
2023-03-18 20:22:10 +0900

    Update MenuManager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,8 +1,7 @@
 
 public class MenuManager {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {		
 		System.out.println("1. Add Subject");
 		System.out.println("2. Delete Subject");
 		System.out.println("3. Edit Subject");
