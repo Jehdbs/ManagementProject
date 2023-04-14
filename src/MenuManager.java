b839e05436ffdf3e24fe7e1c458098c790c8ba7c
jehdbs <bigdoyun@naver.com>
2023-04-14 23:31:06 +0900

    added MenuManager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
 
 public class MenuManager {
-
-	public static void main(String[] args) {		
-		System.out.println("1. Add Subject");
-		System.out.println("2. Delete Subject");
-		System.out.println("3. Edit Subject");
-		System.out.println("4. View Subject");
-		System.out.println("5. Exit");
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		AccountManager accountManager = new AccountManager(input);  
+		int num = -1;
+		
+		while(num != 5) {
+			System.out.println("1. Earning");
+			System.out.println("2. delete");
+			System.out.println("3. Edit Accountbook");
+			System.out.println("4. View Accountbook");
+			System.out.println("5. Exit");
+			System.out.print("Select one number between 1~5 : ");	
+			num = input.nextInt();
+			
+			if (num==1) {
+				accountManager.earning();}
+			else if (num==2) {
+				accountManager.delete();}
+			else if (num==3) {
+				accountManager.editAccountbook();}
+			else if (num==4) {
+				accountManager.viewAccountbook();}
+			else {
+				continue;}
 	}
 
+	}
 }
