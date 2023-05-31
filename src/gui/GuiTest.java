b3a411450b471c12022e034fd2ab8b6f736ace59
jehdbs <bigdoyun@naver.com>
2023-05-31 09:54:53 +0900

    added to GuiTest.java

--- src/gui/GuiTest.java
+++ src/gui/GuiTest.java
@@ -3,7 +3,6 @@ package gui;
 public class GuiTest {
 
 	public static void main(String[] args) {
-		
 		MenuSelection menuselection = new MenuSelection();
 		Earning earning = new Earning();
 		AccountViewer accountViewer = new AccountViewer();
