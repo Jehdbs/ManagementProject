0499f27693de1e22b8b2ce1abdb817aa79397956
jehdbs <bigdoyun@naver.com>
2023-05-31 09:57:51 +0900

    Merge branch 'main' of https://github.com/Jehdbs/ManagementProject.git into main added to GuiTest.java

--- src/gui/GuiTest.java
+++ src/gui/GuiTest.java
@@ -3,6 +3,7 @@ package gui;
 public class GuiTest {
 
 	public static void main(String[] args) {
+		
 		MenuSelection menuselection = new MenuSelection();
 		Earning earning = new Earning();
 		AccountViewer accountViewer = new AccountViewer();
