980b0d04127fa8ee004213a016330c854ff19ecc
jehdbs <bigdoyun@naver.com>
2023-05-31 09:41:26 +0900

    added to AccountViewer.java added to GuiTest.java

--- src/gui/GuiTest.java
+++ src/gui/GuiTest.java
@@ -4,6 +4,9 @@ public class GuiTest {
 
 	public static void main(String[] args) {
 		MenuSelection menuselection = new MenuSelection();
+		Earning earning = new Earning();
+		AccountViewer accountViewer = new AccountViewer();
+		
 	}
 
 }
